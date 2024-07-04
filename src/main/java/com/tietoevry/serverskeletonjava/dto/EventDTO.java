4e840be8f37f71291ed4c4ed9885dd215a20f007
yahya <mohamed.yahya.maad@tietoevry.com>
2024-07-04 10:03:30 +0200

    Start på forslag

--- src/main/java/com/tietoevry/serverskeletonjava/dto/EventDTO.java
+++ src/main/java/com/tietoevry/serverskeletonjava/dto/EventDTO.java
@@ -1,6 +1,8 @@
 package com.tietoevry.serverskeletonjava.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class EventDTO implements IEventDTO{
     @JsonProperty("socSecNum")
     private String socSecNum;
@@ -12,57 +14,4 @@ public class EventDTO implements IEventDTO{
     private String sequenceNumber;
     @JsonProperty("timestamp")
     private String timestamp;
-
-    public EventDTO(){
-
-    }
-
-    public EventDTO(String socSecNum, String eventType, Object value, String sequenceNumber, String timestamp){
-    
-        this.socSecNum = socSecNum;
-        this.eventType = eventType;
-        this.value = value;
-        this.sequenceNumber = sequenceNumber;
-        this.timestamp = timestamp;
-    }
-    public String getSocSecNum(){
-        return socSecNum;
-    }
-    public String getEventType(){
-        return eventType;
-    }
-    public Object getValue(){
-        return value;
-    }
-    public String getSequenceNumber(){
-        return sequenceNumber;
-    }
-    public String getTimestamp(){
-        return timestamp;
-    }
-    public void setSocSecNum(String socSecNum){
-        this.socSecNum = socSecNum;
-    }
-    public void setEventType(String eventType){
-        this.eventType = eventType;
-    }
-    public void setValue(Object value){
-        this.value = value;
-    }
-    public void setSequenceNumber(String sequenceNumber){
-        this.sequenceNumber = sequenceNumber;
-    }
-    public void setTimestamp(String timestamp){
-        this.timestamp = timestamp;
-    }
-    @Override
-    public String toString() {
-        return "EventDTO{" +
-                "socSecNum='" + socSecNum + '\'' +
-                ", eventType='" + eventType + '\'' +
-                ", value=" + value +
-                ", sequenceNumber=" + sequenceNumber +
-                ", timestamp='" + timestamp + '\'' +
-                '}';
-        }
-    }
+}
